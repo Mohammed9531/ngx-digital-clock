5f0a13d49da0e7779a76fc98d0e2dad71ef1675d
Shoukath Mohammed <mohd.shoukath7866@gmail.com>
2018-08-27 21:49:18 -0500

    chore(services): added utility service.

--- src/app/digital-clock/digital-clock.component.ts
+++ src/app/digital-clock/digital-clock.component.ts
@@ -3,6 +3,9 @@ import { Observable, Subscription, timer } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DAYS_SHORT, DAYS_LONG, CLASS_LIST, WATCH_CONFIG } from './../shared/constants/calendar.constants';
 
+/**
+ * @author: Shoukath Mohammed
+ */
 @Component({
   selector: 'app-digital-clock',
   templateUrl: './digital-clock.component.html',
@@ -21,35 +24,41 @@ export class DigitalClockComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     this.subscriptions.push(
       timer(0, 1000)
-      .subscribe((t) => {
-        this.fetchDateTime();
-      })
+        .subscribe((t) => {
+          this.fetchDateTime();
+        })
     );
   }
 
   public fetchDateTime(): void {
-    let now: string[] =  moment().format(WATCH_CONFIG.format).split(' ');
+    let now: string[] = moment().format(WATCH_CONFIG.format).split(' ');
 
     this.dateTimeObj = {};
-    for ( let i = 0; i < now.length; i++ ) {
+    for (let i = 0; i < now.length; i++) {
       this.dateTimeObj[WATCH_CONFIG.keys[i]] = now[i];
     }
     this.constructWatch();
+
+    // const now: Date = new Date();
+    // const d: string[] = now.toDateString().split(' ');
+    // const t: string[] = now.toLocaleTimeString().split(' ');
   }
 
   public constructWatch(): void {
     const rawStr: string = this.dateTimeObj.time;
 
     this.timeFormatList = [];
-    for ( let i = 0; i < rawStr.length; i++ ) {
-       if (rawStr[i] === WATCH_CONFIG.parsers.dots) {
-          this.timeFormatList.push(WATCH_CONFIG.parsers[rawStr[i]]);
-       } else {
-          this.timeFormatList.push(CLASS_LIST[+rawStr[i]]);
-       }
+    for (let i = 0; i < rawStr.length; i++) {
+      if (rawStr[i] === WATCH_CONFIG.parsers.dots) {
+        this.timeFormatList.push(WATCH_CONFIG.parsers[rawStr[i]]);
+      } else {
+        this.timeFormatList.push(CLASS_LIST[+rawStr[i]]);
+      }
     }
-      console.log(this.dateTimeObj);
+    console.log(this.dateTimeObj);
   }
+
+
 
   public ngOnDestroy(): void {
     for (let subscription of this.subscriptions) {
---
+++ src/app/shared/services/util/util.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * @author: Shoukath Mohammed
+ */
+@Injectable()
+export class UtilService {
+
+  constructor() { }
+}
